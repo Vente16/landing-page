5769da038b680f6136d7ba08aaff6292fa2d10d9
jvente <jvente18@gmail.com>
2020-09-17 13:08:31 -0500

    responsive landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./public/login/login.component";
 import { LandingComponent } from "./public/landing/landing.component";
+import {
+  APP_BASE_HREF,
+  LocationStrategy,
+  HashLocationStrategy,
+} from "@angular/common";
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, LandingComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
-  providers: [],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: "" },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/public/landing/landing.component.ts
+++ src/app/public/landing/landing.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-landing',
-  templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  selector: "app-landing",
+  templateUrl: "./landing.component.html",
+  styleUrls: ["./landing.component.css"],
 })
 export class LandingComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onAnchorClick(e) {
+    console.log(e);
   }
-
 }
